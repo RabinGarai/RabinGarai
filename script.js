df6ba5e14573ab8f9ff9973723d155aaf7ac50f9
RabinGarai <rabingarai11@gmail.com>
2025-01-27 13:32:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Example script for form submission or any other functionality
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your message!');
+});
+
+
+
+
+
+
+
